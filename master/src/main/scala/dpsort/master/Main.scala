7348787449dfe85b423c0e33131ae4c91d89f179
Jinho Ko <jinho.ko@postech.ac.kr>
2020-11-11 13:40:59 +0900

    ADD log4j configuration, settings all done

--- master/src/main/scala/dpsort/master/Main.scala
+++ master/src/main/scala/dpsort/master/Main.scala
@@ -1,14 +1,22 @@
 package dpsort.master
 
 import org.apache.logging.log4j.scala.Logging
+import org.apache.logging.log4j.Level
 
 
 object Main extends Logging {
   def main(args: Array[String]): Unit = {
-    def a (): Unit = {
+    def a (i : Int): Unit = {
+      if (i==0) return
       logger.info("testlog!!")
+      a(i-1)
     }
-    printf("hehe")
-    a
+    logger.info("testlog!!")
+    logger.info("testlog!!")
+    logger.info("testlog!!")
+    logger.fatal("log4j:logger.fatal()")
+    logger.fatal("log4j:logger.fatal()")
+
+    a (1000000000)
   }
 }
