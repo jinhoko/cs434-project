627edae5fa391795dcb2d09b0a33c72368769828
Jinho Ko <jinho.ko@postech.ac.kr>
2020-11-13 14:51:19 +0900

    DELETE large example files

--- master/src/main/scala/dpsort/master/Main.scala
+++ master/src/main/scala/dpsort/master/Main.scala
@@ -1,11 +1,13 @@
 package dpsort.master
 
 import org.apache.logging.log4j.scala.Logging
-import org.apache.logging.log4j.Level
 
 
 object Main extends Logging {
   def main(args: Array[String]): Unit = {
+    logger.info("dpsort master starting")
+    // TODO execute master context
+    logger.info("dpsort master finished")
 
     // Load Conf object
 
